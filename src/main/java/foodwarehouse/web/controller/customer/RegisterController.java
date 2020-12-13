25055279e3956ef362217f589106cdd0e7bad042
GieeFoR <kamilooo1998@onet.pl>
2020-12-13 15:55:32 +0100

    Database exception and divided controllers

--- src/main/java/foodwarehouse/web/controller/customer/RegisterController.java
+++ src/main/java/foodwarehouse/web/controller/customer/RegisterController.java
@@ -1,63 +1,73 @@
-package foodwarehouse.web.user;
+package foodwarehouse.web.controller.customer;
 
+import foodwarehouse.core.address.Address;
+import foodwarehouse.core.user.Permission;
 import foodwarehouse.core.user.User;
+import foodwarehouse.core.user.UserService;
 import foodwarehouse.core.user.customer.Customer;
+import foodwarehouse.web.common.SuccessResponse;
+import foodwarehouse.web.error.DatabaseException;
+import foodwarehouse.web.error.RestException;
 import foodwarehouse.web.request.CheckEmailRequest;
 import foodwarehouse.web.request.CheckUsernameRequest;
 import foodwarehouse.web.request.CreateCustomerRequest;
-import foodwarehouse.web.request.CreateUserRequest;
 import foodwarehouse.web.response.CheckEmailResponse;
 import foodwarehouse.web.response.CheckUsernameResponse;
 import foodwarehouse.web.response.RegistrationResponse;
-import foodwarehouse.web.response.UserResponse;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-import foodwarehouse.core.user.UserService;
-import foodwarehouse.core.user.Permission;
-import foodwarehouse.web.common.SuccessResponse;
-import foodwarehouse.web.error.RestException;
-import foodwarehouse.core.address.Address;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.text.html.Option;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
-//@RequestMapping("/users")
-public class UserController {
+public class RegisterController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public RegisterController(UserService userService) {
         this.userService = userService;
     }
 
-
+    //check if username is not used
     @PostMapping("/register/username")
     public SuccessResponse<CheckUsernameResponse> checkUsername(@RequestBody CheckUsernameRequest loginUsername) {
         boolean exists = userService.findByUsername(loginUsername.username()).isPresent();
         return new SuccessResponse<>(new CheckUsernameResponse(exists));
     }
 
+    //check if email is not used
     @PostMapping("/register/email")
     public SuccessResponse<CheckEmailResponse> checkEmail(@RequestBody CheckEmailRequest loginEmail) {
         boolean exists = userService.findByEmail(loginEmail.email()).isPresent();
         return new SuccessResponse<>(new CheckEmailResponse(exists));
     }
 
-    @PostMapping("/register")
+    //register user function
+    @RequestMapping("/register")
     public SuccessResponse<RegistrationResponse> register(@RequestBody CreateCustomerRequest createCustomerRequest) {
-        Optional <User> user = userService.createUser(
+
+        if(!userService.checkConnection()) {
+            String exceptionMessage = "Cannot connect to database.";
+            System.out.println(exceptionMessage);
+            throw new DatabaseException(exceptionMessage);
+        }
+
+        //add user to database
+        Optional<User> user = userService.createUser(
                 createCustomerRequest.account().username(),
                 createCustomerRequest.account().password(),
                 createCustomerRequest.account().email(),
                 Permission.CUSTOMER);
 
+        //when user was not added to database
         if(user.isEmpty()) {
+            //response error
             throw new RestException("Unable to create a new user.");
         }
 
+        //add address to database
         Optional <Address> address = userService.createAddress(
                 createCustomerRequest.address().country(),
                 createCustomerRequest.address().town(),
@@ -66,12 +76,16 @@ public class UserController {
                 createCustomerRequest.address().street(),
                 createCustomerRequest.address().apartmentNumber());
 
+        //when address was not added to database
         if(address.isEmpty()) {
-            userService.deleteUser(user.get());
-            throw new RestException("Unable to create a new address.");
             //delete user from database
+            userService.deleteUser(user.get());
+            //response error
+            throw new RestException("Unable to create a new address.");
+
         }
 
+        //add customer to database
         Optional <Customer> customer = userService.createCustomer(
                 user.get(),
                 address.get(),
@@ -81,39 +95,17 @@ public class UserController {
                 createCustomerRequest.personalData().phoneNumber(),
                 createCustomerRequest.personalData().tax_id());
 
+        //when customer was not added to database
         if(customer.isEmpty()) {
+            //delete user from database
             userService.deleteUser(user.get());
+            //delete address from database
             userService.deleteAddress(address.get());
+            //response error
             throw new RestException("Unable to create a new customer.");
-            //delete user and address from database
         }
+
+        //response on success
         return new SuccessResponse<>(new RegistrationResponse(true));
-    }
-
-    @PreAuthorize("hasRole('Admin')")
-    @GetMapping("/users")
-    public SuccessResponse<List<UserResponse>> getUsers() {
-        final var users = userService
-                .getUsers()
-                .stream()
-                .map(user -> new UserResponse(user.permission().value(), user.userId(), user.username()))
-                .collect(Collectors.toList());
-        return new SuccessResponse<>(users);
-    }
-
-    @PreAuthorize("hasRole('Admin')")
-    @PostMapping("/users")
-    public SuccessResponse<UserResponse> createUser(@RequestBody CreateUserRequest request) {
-        Optional<Permission> permission = Permission.from(request.account().permission());
-
-        if(permission.isEmpty()) {
-            throw new RestException("Wrong permission name.");
-        }
-        else {
-            return userService
-                    .createUser(request.account().username(), request.account().password(), request.account().email(), Permission.from(request.account().permission()).get())
-                    .map(user -> new SuccessResponse<>(new UserResponse(user.permission().value(), user.userId(), user.username())))
-                    .orElseThrow(() -> new RestException("Unable to create a new user."));
-        }
     }
 }
