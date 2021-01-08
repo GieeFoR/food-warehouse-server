a16f90e0b6e41857555fec4716c564dd5e92eb88
GieeFoR <kamilooo1998@onet.pl>
2021-01-08 20:00:00 +0100

    Changed link /car to /vehicle

--- src/main/java/foodwarehouse/web/controller/CarController.java
+++ src/main/java/foodwarehouse/web/controller/CarController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/car")
+@RequestMapping("/vehicle")
 public class CarController {
 
     private final CarService carService;
