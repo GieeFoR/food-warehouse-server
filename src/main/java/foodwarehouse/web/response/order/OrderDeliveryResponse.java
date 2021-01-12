1cdd5f1e7ed975128d6d342b58ee1271b85ac901
GieeFoR <kamilooo1998@onet.pl>
2021-01-12 02:54:20 +0100

    SimpleDateFormat to OrderResponse

--- src/main/java/foodwarehouse/web/response/order/OrderDeliveryResponse.java
+++ src/main/java/foodwarehouse/web/response/order/OrderDeliveryResponse.java
@@ -4,19 +4,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import foodwarehouse.core.data.delivery.Delivery;
 import foodwarehouse.web.response.address.AddressResponse;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public record OrderDeliveryResponse(
         @JsonProperty(value = "delivery_id", required = true)           int deliveryId,
-        @JsonProperty(value = "remove_from_storage_date")               Date removeFromStorage,
-        @JsonProperty(value = "delivery_date")                          Date deliveryDate,
+        @JsonProperty(value = "remove_from_storage_date")               String removeFromStorage,
+        @JsonProperty(value = "delivery_date")                          String deliveryDate,
         @JsonProperty(value = "address", required = true)               AddressResponse addressResponse) {
 
     public static OrderDeliveryResponse fromDelivery(Delivery delivery) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return new OrderDeliveryResponse(
                 delivery.deliveryId(),
-                delivery.removalFromStorage(),
-                delivery.deliveryDate(),
+                sdf.format(delivery.removalFromStorage()),
+                sdf.format(delivery.deliveryDate()),
                 AddressResponse.fromAddress(delivery.address()));
     }
 }
--- src/main/java/foodwarehouse/web/response/order/OrderProductResponse.java
+++ src/main/java/foodwarehouse/web/response/order/OrderProductResponse.java
@@ -3,23 +3,24 @@ package foodwarehouse.web.response.order;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import foodwarehouse.core.data.productBatch.ProductBatch;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public record OrderProductResponse(
         @JsonProperty(value = "product_id", required = true)        int productId,
         @JsonProperty(value = "name", required = true)              String name,
         @JsonProperty(value = "sell_price", required = true)        float sellPrice,
-        @JsonProperty(value = "due_to", required = true)            Date eatByDate,
+        @JsonProperty(value = "due_to", required = true)            String eatByDate,
         @JsonProperty(value = "image", required = true)             String image,
         @JsonProperty(value = "producer_name", required = true)     String makerName,
         @JsonProperty(value = "quantity", required = true)          int quantity) {
 
     public static OrderProductResponse fromProductBatch(ProductBatch productBatch, int quantity) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return new OrderProductResponse(
                 productBatch.product().productId(),
                 productBatch.product().name(),
                 productBatch.product().sellPrice() * (100 - productBatch.discount()) / 100,
-                productBatch.eatByDate(),
+                sdf.format(productBatch.eatByDate()),
                 productBatch.product().image(),
                 productBatch.product().maker().firmName(),
                 quantity);
